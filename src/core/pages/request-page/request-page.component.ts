0cc67f780431e7fb589f12aa0df986306bceebf5
Ivonne <ivonne.morenoa@hotmail.com>
2023-07-18 18:47:40 -0600

    updates

--- src/core/pages/request-page/request-page.component.ts
+++ src/core/pages/request-page/request-page.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 
+export interface RequestForm{
+  name: FormControl<string | null>;
+  phone: FormControl<string | null>;
+  date: FormControl<string | null>;
+  hour: FormControl<string | null>;
+  }
+
+  export interface RequestFormValue {
+    name: string;
+    phone: string;
+    date: string;
+    hour: string;
+  }
 @Component({
   selector: 'app-request-page',
   templateUrl: './request-page.component.html',
   styleUrls: ['./request-page.component.css']
 })
+
 export class RequestPageComponent {
 
+  // public formrequest: FormGroup<RequestForm>= this.fb.group<R
 }

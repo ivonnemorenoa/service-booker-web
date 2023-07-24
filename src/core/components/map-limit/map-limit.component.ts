20293fc993d2512ed45ae5fc5a5a1dbc22f72389
SamuelCastilloRdz <samuel.castillo.rdz1@gmail.com>
2023-07-24 16:19:26 -0600

    map location created

--- src/core/components/map-limit/map-limit.component.ts
+++ src/core/components/map-limit/map-limit.component.ts
@@ -10,16 +10,20 @@ export class MapLimitComponent {
 
 
   ngAfterViewInit() {
-    const map: any = L.map('map').setView([25.6558881, -100.2644069], 13);
+    const map: any = L.map('map').setView([25.6558881, -100.2644069], 10);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    L.marker([25.6558881, -100.2644069]).addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
+    const circle: any = L.circle([25.6968642,-100.2751522], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 23000
+  }).addTo(map);
+
   }
 
 }
